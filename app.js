92fe28509c2e06b37891f89e674fa2a8280f08e7
Benjamin Steidl <localartpool@gmail.com>
2020-07-06 14:55:44 +0200

    Started with CRUD and animation

--- app.js
+++ app.js
@@ -2,10 +2,12 @@
 const todoInput = document.querySelector(`.todo-input`);
 const todoButton = document.querySelector(`.todo-button`);
 const todoList = document.querySelector(`.todo-list`);
+const filterOption = document.querySelector(".filter-todo");
 
 //EventListener
 todoButton.addEventListener(`click`, addTodo);
-
+todoList.addEventListener(`click`, deleteCheck);
+filterOption.addEventListener(`click`, filterTodo);
 //Function
 
 function addTodo(event) {
@@ -16,7 +18,7 @@ function addTodo(event) {
   todoDiv.classList.add(`todo`);
   //Create LI
   const newTodo = document.createElement(`li`);
-  newTodo.innerText = `hey`;
+  newTodo.innerText = todoInput.value;
   newTodo.classList.add(`todo-item`);
   todoDiv.appendChild(newTodo);
   //CHECK MARK BUTTON
@@ -31,4 +33,48 @@ function addTodo(event) {
   todoDiv.appendChild(trashButton);
   //APPEND TO LIST
   todoList.appendChild(todoDiv);
+  //Clear Todo Input VALUE
+  todoInput.value = "";
+}
+
+function deleteCheck(e){
+  const item = e.target;
+  if(item.classList[0] === "trash-btn") {
+    const todo = item.parentElement;
+    //Animation
+    todo.classList.add("fall");
+    todo.addEventListener(`transitionend`, function(){
+      todo.remove();
+    });
+  }
+//Check Mark
+if(item.classList[0] === "complete-btn") {
+  const todo = item.parentElement;
+  todo.classList.toggle(`completed`);
+  }
+}
+
+function filterTodo(e) {
+  const todos = todoList.childNodes;
+  todos.forEach(function(todo) {
+    switch (e.target.value){
+      case "all":
+      todo.style.display = "flex";
+      break;
+      case "completed":
+        if (todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+      case "uncompleted":
+        if(!todo.classList.contains("completed")) {
+          todo.style.display = "flex";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+    }
+  });
 }
